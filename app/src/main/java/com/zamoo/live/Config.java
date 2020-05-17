8f8540dbe4ab346ca91e226c689221bf80801400
Abdul Mannan <mannanzinat@gmail.com>
2020-05-17 10:49:21 +0600

    Updated API details

--- app/src/main/java/com/zamoo/live/Config.java
+++ app/src/main/java/com/zamoo/live/Config.java
@@ -3,13 +3,13 @@ package com.zamoo.live;
 public class Config {
 
     // copy your api url from php admin dashboard & paste below
-    public static final String API_SERVER_URL = "http://demo.redtvlive.com/munda/api/";
+    public static final String API_SERVER_URL = "http://zamoo.tv/api/";
 
     //copy your api key from php admin dashboard & paste below
     public static final String API_KEY = "nw7ugabtx1yndfo7eut1mk1a";
 
     //copy your terms url from php admin dashboard & paste below
-    public static final String TERMS_URL = "http://demo.redtvlive.com/subscription/page/terms-condition";
+    public static final String TERMS_URL = "http://zamoo.tv/terms/";
 
     //default theme
     public static boolean DEFAULT_DARK_THEME_ENABLE = true;
